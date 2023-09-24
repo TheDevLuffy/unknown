32b4515ce126e1040a66529f172a8c2d4c7ce78e
thedevluffy <thedevluffy@gmail.com>
2023-09-24 20:21:01 +0900

    setup docker

---
+++ unknown/docker/init.sh
@@ -0,0 +1,8 @@
+mkdir ./volume
+mkdir ./volume/zookeeper
+mkdir ./volume/zookeeper/zoo1
+mkdir ./volume/zookeeper/zoo2
+mkdir ./volume/zookeeper/zoo3
+mkdir ./volume/kafka
+mkdir ./volume/kafka/data
+mkdir ./volume/kafka/logs
